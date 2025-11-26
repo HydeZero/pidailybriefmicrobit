ae60a82dad6e7bced08073fa088025078aebef21
Zachary Smith <zach10smith@proton.me>
2025-11-25 19:17:19 -0600

    Attempt to move split pi-side

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def render_stuff():
     global answer
     answer = serial.read_line()
     I2C_LCD1602.clear()
-    answer_split = answer.split(";")
+    answer_split = answer.split("!@;")
     if answer_split[0] == "weather":
         I2C_LCD1602.clear()
         I2C_LCD1602.show_string(answer_split[1], 0, 0)
@@ -10,10 +10,11 @@ def render_stuff():
     if answer_split[0] == "news":
         try:
             split_words = split_words_to_fit(answer_split[1])
-            basic.show_string(split_words[0])
             I2C_LCD1602.show_string(split_words[0], 0, 0)
-            if len(split_words) == 2:
+            if len(split_words) >= 2:
                 I2C_LCD1602.show_string(split_words[1], 0, 1)
+            if len(split_words) == 3:
+                pass
         except:
             basic.showString("ERROR! ERROR! ERROR! ERROR!")
             control.reset()
@@ -21,19 +22,6 @@ def render_stuff():
 def on_button_pressed_a():
     get_stuff("weather")
 input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def split_words_to_fit(in_words: str):
-    in_split = in_words.split(" ")
-    out_words = []
-    temp = ""
-    for word in in_split:
-        if len(temp) + len(word) > 16:
-            out_words.append(temp)
-            temp = word + " "
-        else:
-            temp = temp + word + " "
-    out_words.append(temp) # append temp since string has ended
-    return out_words
 
 
 def get_stuff(mode: str):
@@ -49,14 +37,6 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 answer = ""
 I2C_LCD1602.lcd_init(39)
 I2C_LCD1602.clear()
-I2C_LCD1602.show_string("Hello", 0, 0)
-basic.pause(1000)
-I2C_LCD1602.show_string("This is your", 0, 0)
-I2C_LCD1602.show_string("Pi Brief", 0, 1)
-basic.pause(5000)
-I2C_LCD1602.clear()
-I2C_LCD1602.show_string("Waiting for Pi", 0, 0)
-I2C_LCD1602.show_string("connection...", 0, 1)
-serial.set_rx_buffer_size(64)
-serial.set_tx_buffer_size(64)
+serial.set_rx_buffer_size(128)
+serial.set_tx_buffer_size(128)
 basic.forever(render_stuff)
--- main.ts
+++ main.ts
@@ -6,6 +6,7 @@ function split_words_to_fit(in_words: string): string[] {
     let out_words = []
     let temp = ""
     for (let word of in_split) {
+        serial.writeNumber(temp.length + word.length)
         if (temp.length + word.length > 16) {
             out_words.push(temp)
             temp = word + " "
@@ -14,7 +15,10 @@ function split_words_to_fit(in_words: string): string[] {
         }
         
     }
-    out_words.push(temp)
+    if (out_words.length < 2) {
+        out_words.push(temp)
+    }
+    
     //  append temp since string has ended
     return out_words
 }
@@ -36,22 +40,14 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
 let answer = ""
 I2C_LCD1602.LcdInit(39)
 I2C_LCD1602.clear()
-I2C_LCD1602.ShowString("Hello", 0, 0)
-basic.pause(1000)
-I2C_LCD1602.ShowString("This is your", 0, 0)
-I2C_LCD1602.ShowString("Pi Brief", 0, 1)
-basic.pause(5000)
-I2C_LCD1602.clear()
-I2C_LCD1602.ShowString("Waiting for Pi", 0, 0)
-I2C_LCD1602.ShowString("connection...", 0, 1)
-serial.setRxBufferSize(64)
-serial.setTxBufferSize(64)
+serial.setRxBufferSize(128)
+serial.setTxBufferSize(128)
 basic.forever(function render_stuff() {
     let split_words: string[];
     
     answer = serial.readLine()
     I2C_LCD1602.clear()
-    let answer_split = _py.py_string_split(answer, ";")
+    let answer_split = _py.py_string_split(answer, "!@;")
     if (answer_split[0] == "weather") {
         I2C_LCD1602.clear()
         I2C_LCD1602.ShowString(answer_split[1], 0, 0)
@@ -61,10 +57,13 @@ basic.forever(function render_stuff() {
     if (answer_split[0] == "news") {
         try {
             split_words = split_words_to_fit(answer_split[1])
-            basic.showString(split_words[0])
             I2C_LCD1602.ShowString(split_words[0], 0, 0)
-            if (split_words.length == 2) {
+            if (split_words.length >= 2) {
                 I2C_LCD1602.ShowString(split_words[1], 0, 1)
+            }
+            
+            if (split_words.length == 3) {
+                
             }
             
         }
